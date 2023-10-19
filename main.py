ab1add603cf6cefde207ca6614cfa5821691fb70
Pidoxy <eemmanuel.idoko@gmail.com>
2023-10-19 09:45:41 +0100

    script updated

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from typing import Optional
+# from typing import Optional
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
 
 from fastapi import FastAPI
+import joblib
 
 app = FastAPI()
 
+#pkl
+phish_model = open('phishing.pkl','rb')
+phish_model_ls = joblib.load(phish_model)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# ML Aspect
+@app.get('/predict/{feature}')
+async def predict(features):
+	X_predict = []
+	X_predict.append(str(features))
+	y_Predict = phish_model_ls.predict(X_predict)
+	if y_Predict == 'bad':
+		result = "This is a Phishing Site"
+	else:
+		result = "This is not a Phishing Site"
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+	return (features, result)
